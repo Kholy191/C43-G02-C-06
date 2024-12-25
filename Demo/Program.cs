2a4b251a85fb7fb8c17f0a9bf4e0e47cf8838082
Ahmed El-Kholy <a.adel27111@gmail.com>
2024-12-25 23:54:57 +0200

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        string ReturnString(string str)
+        {
+            string chars = new string("afafa");  
+            return new string(chars);
+        }
+    }
+}
